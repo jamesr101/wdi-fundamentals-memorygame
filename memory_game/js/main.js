546fbb7f5e69f10211fff9cabf7d46af261ca4ad
James Robins <james@Jamess-MBP-2.home>
2018-07-02 20:12:17 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage:"images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
@@ -11,8 +32,12 @@ var checkForMatch = function(){
 
 var flipCard = function(cardId){
 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+
 
 	if (cardsInPlay.length === 2){
 		checkForMatch();
@@ -22,3 +47,4 @@ var flipCard = function(cardId){
 
 flipCard(0);
 flipCard(2);
+
